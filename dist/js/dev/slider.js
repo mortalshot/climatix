a41739aad4924fb30b4ff70bddd2c6daa86e59cc
mortalshot <seregagl1996@gmail.com>
2025-09-30 12:39:21 +0300

    feat: обновления интерфейса
    - добавил перемотку слайдов в wdgt-equipment__slider
    - поменял ссылки в категориях
    - добавил ссылки в заголовки на таксономии
    - изменил отображение плюсика в spollers

--- dist/js/dev/slider.js
+++ dist/js/dev/slider.js
@@ -666,9 +666,9 @@ var eventsEmitter = {
     if (!self.eventsListeners || self.destroyed) return self;
     if (typeof handler !== "function") return self;
     const method = priority ? "unshift" : "push";
-    events2.split(" ").forEach((event) => {
-      if (!self.eventsListeners[event]) self.eventsListeners[event] = [];
-      self.eventsListeners[event][method](handler);
+    events2.split(" ").forEach((event2) => {
+      if (!self.eventsListeners[event2]) self.eventsListeners[event2] = [];
+      self.eventsListeners[event2][method](handler);
     });
     return self;
   },
@@ -713,13 +713,13 @@ var eventsEmitter = {
     const self = this;
     if (!self.eventsListeners || self.destroyed) return self;
     if (!self.eventsListeners) return self;
-    events2.split(" ").forEach((event) => {
+    events2.split(" ").forEach((event2) => {
       if (typeof handler === "undefined") {
-        self.eventsListeners[event] = [];
-      } else if (self.eventsListeners[event]) {
-        self.eventsListeners[event].forEach((eventHandler, index) => {
+        self.eventsListeners[event2] = [];
+      } else if (self.eventsListeners[event2]) {
+        self.eventsListeners[event2].forEach((eventHandler, index) => {
           if (eventHandler === handler || eventHandler.__emitterProxy && eventHandler.__emitterProxy === handler) {
-            self.eventsListeners[event].splice(index, 1);
+            self.eventsListeners[event2].splice(index, 1);
           }
         });
       }
@@ -747,14 +747,14 @@ var eventsEmitter = {
     }
     data.unshift(context);
     const eventsArray = Array.isArray(events2) ? events2 : events2.split(" ");
-    eventsArray.forEach((event) => {
+    eventsArray.forEach((event2) => {
       if (self.eventsAnyListeners && self.eventsAnyListeners.length) {
         self.eventsAnyListeners.forEach((eventHandler) => {
-          eventHandler.apply(context, [event, ...data]);
+          eventHandler.apply(context, [event2, ...data]);
         });
       }
-      if (self.eventsListeners && self.eventsListeners[event]) {
-        self.eventsListeners[event].forEach((eventHandler) => {
+      if (self.eventsListeners && self.eventsListeners[event2]) {
+        self.eventsListeners[event2].forEach((eventHandler) => {
           eventHandler.apply(context, data);
         });
       }
@@ -2495,7 +2495,7 @@ function closestElement(selector, base) {
   }
   return __closestFrom(base);
 }
-function preventEdgeSwipe(swiper, event, startX) {
+function preventEdgeSwipe(swiper, event2, startX) {
   const window2 = getWindow();
   const {
     params
@@ -2504,17 +2504,17 @@ function preventEdgeSwipe(swiper, event, startX) {
   const edgeSwipeThreshold = params.edgeSwipeThreshold;
   if (edgeSwipeDetection && (startX <= edgeSwipeThreshold || startX >= window2.innerWidth - edgeSwipeThreshold)) {
     if (edgeSwipeDetection === "prevent") {
-      event.preventDefault();
+      event2.preventDefault();
       return true;
     }
     return false;
   }
   return true;
 }
-function onTouchStart(event) {
+function onTouchStart(event2) {
   const swiper = this;
   const document2 = getDocument();
-  let e = event;
+  let e = event2;
   if (e.originalEvent) e = e.originalEvent;
   const data = swiper.touchEventsData;
   if (e.type === "pointerdown") {
@@ -2603,7 +2603,7 @@ function onTouchStart(event) {
   }
   swiper.emit("touchStart", e);
 }
-function onTouchMove(event) {
+function onTouchMove(event2) {
   const document2 = getDocument();
   const swiper = this;
   const data = swiper.touchEventsData;
@@ -2614,8 +2614,8 @@ function onTouchMove(event) {
     enabled
   } = swiper;
   if (!enabled) return;
-  if (!params.simulateTouch && event.pointerType === "mouse") return;
-  let e = event;
+  if (!params.simulateTouch && event2.pointerType === "mouse") return;
+  let e = event2;
   if (e.originalEvent) e = e.originalEvent;
   if (e.type === "pointermove") {
     if (data.touchId !== null) return;
@@ -2852,10 +2852,10 @@ function onTouchMove(event) {
   swiper.updateProgress(data.currentTranslate);
   swiper.setTranslate(data.currentTranslate);
 }
-function onTouchEnd(event) {
+function onTouchEnd(event2) {
   const swiper = this;
   const data = swiper.touchEventsData;
-  let e = event;
+  let e = event2;
   if (e.originalEvent) e = e.originalEvent;
   let targetTouch;
   const isTouchEvent = e.type === "touchend" || e.type === "touchcancel";
@@ -4146,6 +4146,305 @@ Object.keys(prototypes).forEach((prototypeGroup) => {
   });
 });
 Swiper.use([Resize, Observer]);
+function Mousewheel(_ref) {
+  let {
+    swiper,
+    extendParams,
+    on,
+    emit
+  } = _ref;
+  const window2 = getWindow();
+  extendParams({
+    mousewheel: {
+      enabled: false,
+      releaseOnEdges: false,
+      invert: false,
+      forceToAxis: false,
+      sensitivity: 1,
+      eventsTarget: "container",
+      thresholdDelta: null,
+      thresholdTime: null,
+      noMousewheelClass: "swiper-no-mousewheel"
+    }
+  });
+  swiper.mousewheel = {
+    enabled: false
+  };
+  let timeout;
+  let lastScrollTime = now();
+  let lastEventBeforeSnap;
+  const recentWheelEvents = [];
+  function normalize(e) {
+    const PIXEL_STEP = 10;
+    const LINE_HEIGHT = 40;
+    const PAGE_HEIGHT = 800;
+    let sX = 0;
+    let sY = 0;
+    let pX = 0;
+    let pY = 0;
+    if ("detail" in e) {
+      sY = e.detail;
+    }
+    if ("wheelDelta" in e) {
+      sY = -e.wheelDelta / 120;
+    }
+    if ("wheelDeltaY" in e) {
+      sY = -e.wheelDeltaY / 120;
+    }
+    if ("wheelDeltaX" in e) {
+      sX = -e.wheelDeltaX / 120;
+    }
+    if ("axis" in e && e.axis === e.HORIZONTAL_AXIS) {
+      sX = sY;
+      sY = 0;
+    }
+    pX = sX * PIXEL_STEP;
+    pY = sY * PIXEL_STEP;
+    if ("deltaY" in e) {
+      pY = e.deltaY;
+    }
+    if ("deltaX" in e) {
+      pX = e.deltaX;
+    }
+    if (e.shiftKey && !pX) {
+      pX = pY;
+      pY = 0;
+    }
+    if ((pX || pY) && e.deltaMode) {
+      if (e.deltaMode === 1) {
+        pX *= LINE_HEIGHT;
+        pY *= LINE_HEIGHT;
+      } else {
+        pX *= PAGE_HEIGHT;
+        pY *= PAGE_HEIGHT;
+      }
+    }
+    if (pX && !sX) {
+      sX = pX < 1 ? -1 : 1;
+    }
+    if (pY && !sY) {
+      sY = pY < 1 ? -1 : 1;
+    }
+    return {
+      spinX: sX,
+      spinY: sY,
+      pixelX: pX,
+      pixelY: pY
+    };
+  }
+  function handleMouseEnter() {
+    if (!swiper.enabled) return;
+    swiper.mouseEntered = true;
+  }
+  function handleMouseLeave() {
+    if (!swiper.enabled) return;
+    swiper.mouseEntered = false;
+  }
+  function animateSlider(newEvent) {
+    if (swiper.params.mousewheel.thresholdDelta && newEvent.delta < swiper.params.mousewheel.thresholdDelta) {
+      return false;
+    }
+    if (swiper.params.mousewheel.thresholdTime && now() - lastScrollTime < swiper.params.mousewheel.thresholdTime) {
+      return false;
+    }
+    if (newEvent.delta >= 6 && now() - lastScrollTime < 60) {
+      return true;
+    }
+    if (newEvent.direction < 0) {
+      if ((!swiper.isEnd || swiper.params.loop) && !swiper.animating) {
+        swiper.slideNext();
+        emit("scroll", newEvent.raw);
+      }
+    } else if ((!swiper.isBeginning || swiper.params.loop) && !swiper.animating) {
+      swiper.slidePrev();
+      emit("scroll", newEvent.raw);
+    }
+    lastScrollTime = new window2.Date().getTime();
+    return false;
+  }
+  function releaseScroll(newEvent) {
+    const params = swiper.params.mousewheel;
+    if (newEvent.direction < 0) {
+      if (swiper.isEnd && !swiper.params.loop && params.releaseOnEdges) {
+        return true;
+      }
+    } else if (swiper.isBeginning && !swiper.params.loop && params.releaseOnEdges) {
+      return true;
+    }
+    return false;
+  }
+  function handle(event2) {
+    let e = event2;
+    let disableParentSwiper = true;
+    if (!swiper.enabled) return;
+    if (event2.target.closest(`.${swiper.params.mousewheel.noMousewheelClass}`)) return;
+    const params = swiper.params.mousewheel;
+    if (swiper.params.cssMode) {
+      e.preventDefault();
+    }
+    let targetEl = swiper.el;
+    if (swiper.params.mousewheel.eventsTarget !== "container") {
+      targetEl = document.querySelector(swiper.params.mousewheel.eventsTarget);
+    }
+    const targetElContainsTarget = targetEl && targetEl.contains(e.target);
+    if (!swiper.mouseEntered && !targetElContainsTarget && !params.releaseOnEdges) return true;
+    if (e.originalEvent) e = e.originalEvent;
+    let delta = 0;
+    const rtlFactor = swiper.rtlTranslate ? -1 : 1;
+    const data = normalize(e);
+    if (params.forceToAxis) {
+      if (swiper.isHorizontal()) {
+        if (Math.abs(data.pixelX) > Math.abs(data.pixelY)) delta = -data.pixelX * rtlFactor;
+        else return true;
+      } else if (Math.abs(data.pixelY) > Math.abs(data.pixelX)) delta = -data.pixelY;
+      else return true;
+    } else {
+      delta = Math.abs(data.pixelX) > Math.abs(data.pixelY) ? -data.pixelX * rtlFactor : -data.pixelY;
+    }
+    if (delta === 0) return true;
+    if (params.invert) delta = -delta;
+    let positions = swiper.getTranslate() + delta * params.sensitivity;
+    if (positions >= swiper.minTranslate()) positions = swiper.minTranslate();
+    if (positions <= swiper.maxTranslate()) positions = swiper.maxTranslate();
+    disableParentSwiper = swiper.params.loop ? true : !(positions === swiper.minTranslate() || positions === swiper.maxTranslate());
+    if (disableParentSwiper && swiper.params.nested) e.stopPropagation();
+    if (!swiper.params.freeMode || !swiper.params.freeMode.enabled) {
+      const newEvent = {
+        time: now(),
+        delta: Math.abs(delta),
+        direction: Math.sign(delta),
+        raw: event2
+      };
+      if (recentWheelEvents.length >= 2) {
+        recentWheelEvents.shift();
+      }
+      const prevEvent = recentWheelEvents.length ? recentWheelEvents[recentWheelEvents.length - 1] : void 0;
+      recentWheelEvents.push(newEvent);
+      if (prevEvent) {
+        if (newEvent.direction !== prevEvent.direction || newEvent.delta > prevEvent.delta || newEvent.time > prevEvent.time + 150) {
+          animateSlider(newEvent);
+        }
+      } else {
+        animateSlider(newEvent);
+      }
+      if (releaseScroll(newEvent)) {
+        return true;
+      }
+    } else {
+      const newEvent = {
+        time: now(),
+        delta: Math.abs(delta),
+        direction: Math.sign(delta)
+      };
+      const ignoreWheelEvents = lastEventBeforeSnap && newEvent.time < lastEventBeforeSnap.time + 500 && newEvent.delta <= lastEventBeforeSnap.delta && newEvent.direction === lastEventBeforeSnap.direction;
+      if (!ignoreWheelEvents) {
+        lastEventBeforeSnap = void 0;
+        let position = swiper.getTranslate() + delta * params.sensitivity;
+        const wasBeginning = swiper.isBeginning;
+        const wasEnd = swiper.isEnd;
+        if (position >= swiper.minTranslate()) position = swiper.minTranslate();
+        if (position <= swiper.maxTranslate()) position = swiper.maxTranslate();
+        swiper.setTransition(0);
+        swiper.setTranslate(position);
+        swiper.updateProgress();
+        swiper.updateActiveIndex();
+        swiper.updateSlidesClasses();
+        if (!wasBeginning && swiper.isBeginning || !wasEnd && swiper.isEnd) {
+          swiper.updateSlidesClasses();
+        }
+        if (swiper.params.loop) {
+          swiper.loopFix({
+            direction: newEvent.direction < 0 ? "next" : "prev",
+            byMousewheel: true
+          });
+        }
+        if (swiper.params.freeMode.sticky) {
+          clearTimeout(timeout);
+          timeout = void 0;
+          if (recentWheelEvents.length >= 15) {
+            recentWheelEvents.shift();
+          }
+          const prevEvent = recentWheelEvents.length ? recentWheelEvents[recentWheelEvents.length - 1] : void 0;
+          const firstEvent = recentWheelEvents[0];
+          recentWheelEvents.push(newEvent);
+          if (prevEvent && (newEvent.delta > prevEvent.delta || newEvent.direction !== prevEvent.direction)) {
+            recentWheelEvents.splice(0);
+          } else if (recentWheelEvents.length >= 15 && newEvent.time - firstEvent.time < 500 && firstEvent.delta - newEvent.delta >= 1 && newEvent.delta <= 6) {
+            const snapToThreshold = delta > 0 ? 0.8 : 0.2;
+            lastEventBeforeSnap = newEvent;
+            recentWheelEvents.splice(0);
+            timeout = nextTick(() => {
+              if (swiper.destroyed || !swiper.params) return;
+              swiper.slideToClosest(swiper.params.speed, true, void 0, snapToThreshold);
+            }, 0);
+          }
+          if (!timeout) {
+            timeout = nextTick(() => {
+              if (swiper.destroyed || !swiper.params) return;
+              const snapToThreshold = 0.5;
+              lastEventBeforeSnap = newEvent;
+              recentWheelEvents.splice(0);
+              swiper.slideToClosest(swiper.params.speed, true, void 0, snapToThreshold);
+            }, 500);
+          }
+        }
+        if (!ignoreWheelEvents) emit("scroll", e);
+        if (swiper.params.autoplay && swiper.params.autoplay.disableOnInteraction) swiper.autoplay.stop();
+        if (params.releaseOnEdges && (position === swiper.minTranslate() || position === swiper.maxTranslate())) {
+          return true;
+        }
+      }
+    }
+    if (e.preventDefault) e.preventDefault();
+    else e.returnValue = false;
+    return false;
+  }
+  function events2(method) {
+    let targetEl = swiper.el;
+    if (swiper.params.mousewheel.eventsTarget !== "container") {
+      targetEl = document.querySelector(swiper.params.mousewheel.eventsTarget);
+    }
+    targetEl[method]("mouseenter", handleMouseEnter);
+    targetEl[method]("mouseleave", handleMouseLeave);
+    targetEl[method]("wheel", handle);
+  }
+  function enable() {
+    if (swiper.params.cssMode) {
+      swiper.wrapperEl.removeEventListener("wheel", handle);
+      return true;
+    }
+    if (swiper.mousewheel.enabled) return false;
+    events2("addEventListener");
+    swiper.mousewheel.enabled = true;
+    return true;
+  }
+  function disable() {
+    if (swiper.params.cssMode) {
+      swiper.wrapperEl.addEventListener(event, handle);
+      return true;
+    }
+    if (!swiper.mousewheel.enabled) return false;
+    events2("removeEventListener");
+    swiper.mousewheel.enabled = false;
+    return true;
+  }
+  on("init", () => {
+    if (!swiper.params.mousewheel.enabled && swiper.params.cssMode) {
+      disable();
+    }
+    if (swiper.params.mousewheel.enabled) enable();
+  });
+  on("destroy", () => {
+    if (swiper.params.cssMode) {
+      enable();
+    }
+    if (swiper.mousewheel.enabled) disable();
+  });
+  Object.assign(swiper.mousewheel, {
+    enable,
+    disable
+  });
+}
 function createElementIfNotDefined(swiper, originalParams, params, checkProps) {
   if (swiper.params.createElements) {
     Object.keys(checkProps).forEach((key) => {
@@ -5343,13 +5642,19 @@ function initSliders() {
     document.querySelectorAll(".wdgt-equipment__slider").forEach((wrap) => {
       const swiperEl = wrap.querySelector(".swiper");
       new Swiper(swiperEl, {
-        modules: [Navigation, Pagination],
+        modules: [Navigation, Pagination, Mousewheel],
         observer: true,
         observeParents: true,
         slidesPerView: 1.3,
         spaceBetween: 15,
         speed: 800,
         watchOverflow: true,
+        mousewheel: {
+          forceToAxis: false,
+          releaseOnEdges: true,
+          thresholdDelta: 5,
+          sensitivity: 1
+        },
         // Пагинация
         pagination: {
           el: wrap.querySelector(".swiper-pagination"),
